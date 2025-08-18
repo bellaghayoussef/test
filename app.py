b3100c2b599c3867869c393b1fbd83f341c9335d
bellaghayoussefNadas <youssef.bellagha@nadas-group.com>
2025-08-18 13:11:38 +0100

    first commitc

---
+++ app.py
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+
+app.get('/api/boutiques', async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://192.168.1.85:88/ouch/public/api/boutiques');
+  const content = await page.evaluate(() => {
+    return JSON.parse(document.body.innerText); // extract JSON
+  });
+  await browser.close();
+  res.json(content);
+});
+
+app.listen(3000, () => console.log('Proxy running on port 3000'));
