76e73bafbe9dace57fa45cd211ee2d30f5bcf784
bellaghayoussefNadas <youssef.bellagha@nadas-group.com>
2025-08-18 14:37:34 +0100

    flusk dd new

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def scrape(url):
 
 if __name__ == "__main__":
     # host=0.0.0.0 makes it accessible from network, debug=True for development
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
