090cb5b92e8ac4d0d559bdfbaa882955cac845b5
bellaghayoussefNadas <youssef.bellagha@nadas-group.com>
2025-08-18 13:38:07 +0100

    2222

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ BOUTIQUE_API = "https://vitrina.hstn.me/api/boutiques"
 @app.route("/boutiques")
 def boutiques():
     try:
+        print(BOUTIQUE_API)
         response = requests.get(BOUTIQUE_API, timeout=10)
+
         data = response.json()
+        print(data)
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
