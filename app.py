b5c79bdb25536f100bb15aa2326af5e66e39f764
bellaghayoussefNadas <youssef.bellagha@nadas-group.com>
2025-08-19 12:23:00 +0100

    f

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def scrape(url):
     target_url = f"https://vitrina.hstn.me/api/{url}"  # <-- f-string fixes it
     print(target_url)
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox'])
         print(target_url)
         page = browser.new_page()
         page.goto(target_url)
@@ -26,3 +26,5 @@ def scrape(url):
         # If not JSON, return raw content
         return content
 
+if __name__ == '__main__':
+    app.run(debug=os.environ.get('DEBUG', False), host='0.0.0.0', port=8080)
